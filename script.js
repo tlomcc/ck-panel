17f0a4727bfbc5076aebf32f823a4f0fec23bcff
tlomcc <136969541+tlomcc@users.noreply.github.com>
2026-07-25 18:02:07 +0800

    style: consolidate Memory Loom shell CSS (v144)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var API_KEY_STORAGE='ckMemoryApiKey';
 var API=API_BASE;
 var ENTITY_GRAPH_URL=GRAPH_API_BASE+'/entity-graph';
 var ENTITY_FACTS_URL=GRAPH_API_BASE+'/entity-facts';
-var CK_PANEL_VERSION=window.CK_PANEL_VERSION||'chat-v143-memory-loom-refinement';
+var CK_PANEL_VERSION=window.CK_PANEL_VERSION||'chat-v144-memory-loom-shell';
 var ckPanelUpdateTarget='';
 var ckPanelUpdateMode='update';
 try{
