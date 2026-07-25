48f4b4882791f5094a3a75b2896675699ffbff08
tlomcc <136969541+tlomcc@users.noreply.github.com>
2026-07-25 12:49:08 +0800

    style: tokenize chat debug cards to Memory Loom paper + signal line (v141)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var API_KEY_STORAGE='ckMemoryApiKey';
 var API=API_BASE;
 var ENTITY_GRAPH_URL=GRAPH_API_BASE+'/entity-graph';
 var ENTITY_FACTS_URL=GRAPH_API_BASE+'/entity-facts';
-var CK_PANEL_VERSION=window.CK_PANEL_VERSION||'chat-v140-memory-loom-icons';
+var CK_PANEL_VERSION=window.CK_PANEL_VERSION||'chat-v141-memory-loom-debug-cards';
 var ckPanelUpdateTarget='';
 var ckPanelUpdateMode='update';
 try{
